b760a16444c881df22642a0c9d9561da4ac09b9b
Johan Leandro Porras Galindo <johan.porras@OMNICON.CC>
2018-10-03 17:38:29 -0500

    Completed check  post handler jp

--- config.js
+++ config.js
@@ -9,14 +9,16 @@ enviroments.staging={
   'httpPort':3000,
   'httpsPort':3001,
   'envName': 'staging',
-  'hashingSecret':'thisIsASecret'
+  'hashingSecret':'thisIsASecret',
+  'maxChecks':5
 };
 //Prudciton object
 enviroments.production={
   'httpPort':5000,
   'httpsPort':5001,
   'envName': 'production',
-  'hashingSecret':'thisIsAlsoASecret'
+  'hashingSecret':'thisIsAlsoASecret',
+  'maxChecks':5
 
 };
 
--- index.js
+++ index.js
@@ -115,5 +115,6 @@ var router={
   'ping':handlers.ping,
   'users':handlers.users,
   'tokens':handlers.tokens,
+  'checks':handlers.checks,
   'notFound':handlers.notFound
 };
